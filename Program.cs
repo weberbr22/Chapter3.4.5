c350239e0cae6473c868d31a66b2cf18c7b6a28d
weberbr22 <113095393+weberbr22@users.noreply.github.com>
2022-09-15 12:09:18 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Chapter3._4._5
+{
+    class Program
+    {
+
+        static bool even(int n)
+        {
+            return n % 2 == 0 ? true : false;
+        }
+
+        static bool processor(int n)
+        {
+            return n % 5 && n % 7 == 0 ? true : false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Chapter 3!");
+            int x = 3;
+            int p = 35;
+            Console.WriteLine(even(x));
+
+            Console.WriteLine(processor(p));
+
+        }
+    }
+}
+
