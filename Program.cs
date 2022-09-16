2b5e179032d06fd7e57f0d963131cea7446ef2ce
weberbr22 <113095393+weberbr22@users.noreply.github.com>
2022-09-16 11:16:41 +0100

    Got onto 8

--- Program.cs
+++ Program.cs
@@ -15,17 +15,52 @@ namespace Chapter3._4._5
             return n % 5 == 0 && n % 7 == 0 ? true : false;
         }
 
+        static bool is7(int n)
+        {
+            int x;
+            int y;
+            x = n / 100;
+            y = x % 10;
+            return y == 7 ? true : false;
+        }
+
+        static bool Getbit(int BitNumber)
+        {
+            bool bit = (BitNumber & 8) != 0;
+            return bit;
+        }
+
+        static int Trapeziod(int a, int b, int h)
+        {
+            int area;
+            area = ((a + b) * h) / 2;
+            return area;
+        }
+
+        static int Rectangle()
+        {
+            int a;
+            int b;
+            Console.WriteLine("Enter height: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Width: ");
+            b = Int32.Parse(Console.ReadLine());
+            int area = a * b;
+            return area;
+
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Chapter 3!");
-            int x = 3;
-            int p = 35;
-            Console.WriteLine(even(x));
-
-            Console.WriteLine(processor(p));
-
             //test to see if this has worked on github
             //another test to see if this has worked on github
+            Console.WriteLine(even(3));
+            Console.WriteLine(processor(35));
+            Console.WriteLine(is7(701));
+            Console.WriteLine(Getbit(25));
+            Console.WriteLine(Trapeziod(2, 3, 5));
+            Console.WriteLine(Rectangle());
         }
     }
 }
